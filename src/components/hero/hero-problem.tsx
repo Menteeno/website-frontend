947c3c2d7c6dc26687b374d38af6a9b602ccb827
Saleh <ordinarysaleh@pm.me>
2025-09-28 15:40:32 +0330

    Update components.json to add MagicUI registry, add @radix-ui/react-icons dependency in package.json, and implement BentoGrid and BentoCard components for improved layout in HeroProblem. Refactor HeroProblem component to utilize new Bento components and enhance UI structure.

--- src/components/hero/hero-problem.tsx
+++ src/components/hero/hero-problem.tsx
@@ -3,182 +3,77 @@
 import { useTranslation } from "@/hooks/use-translation";
 import {
   BrainIcon,
-  ChartColumnBigIcon,
   CompassIcon,
   HandshakeIcon,
   HeadsetIcon,
-  MessagesSquareIcon,
   SwordsIcon,
 } from "lucide-react";
-import { MagicCard } from "../magicui/magic-card";
-import { Card, CardContent } from "../ui/card";
+import { BentoCard, BentoGrid } from "../ui/bento-grid";
 
 const HeroProblem = () => {
   const { t } = useTranslation();
 
-  return (
-    <div className="min-h-screen flex gap-8 flex-col items-center justify-center max-w-7xl mx-auto">
-      <h2 className="mt-4 font-bold text-xl text-center sm:text-3xl md:text-4xl leading-relaxed">
-        {t("messages.hero-problem.title")}
-      </h2>
-      <div className="relative px-4 z-10 col-span-7 text-center lg:text-start">
-        <div className="mt-10 grid grid-cols-12 w-full gap-4 items-stretch text-lg text-foreground">
-          <Card className="p-0 col-span-6 shadow-none border-none">
-            <MagicCard
-              gradientColor="oklch(72.3% 0.219 149.579 / .15)"
-              className="p-0 h-full"
-              gradientFrom="oklch(72.3% 0.219 149.579 / .9)"
-              gradientTo="oklch(72.3% 0.219 149.579 / .4)"
-            >
-              <CardContent className="p-4">
-                <div className="relative flex items-center gap-4 mb-4">
-                  <div className="absolute end-0 top-0 size-12 bg-primary/15 rounded-full p-2">
-                    <MessagesSquareIcon className="size-8 text-primary" />
-                  </div>
-                  <h3 className="font-semibold text-2xl text-foreground">
-                    {t("messages.hero-problem.sections.mentorship.title")}
-                  </h3>
-                </div>
-                <p className="ps-1">
-                  {t("messages.hero-problem.sections.mentorship.description")}
-                </p>
-              </CardContent>
-            </MagicCard>
-          </Card>
+  const features = [
+    {
+      Icon: CompassIcon,
+      name: t("messages.hero-problem.sections.learning.title"),
+      description: t("messages.hero-problem.sections.learning.description"),
+      href: "/",
+      cta: "Learn more",
+      background: <div className="absolute -top-20 -right-20 opacity-60" />,
+      className: "lg:row-start-1 lg:row-end-3 lg:col-start-2 lg:col-end-3",
+    },
+    {
+      Icon: HandshakeIcon,
+      name: t("messages.hero-problem.sections.practice.title"),
+      description: t("messages.hero-problem.sections.practice.description"),
+      href: "/",
+      cta: "Learn more",
+      background: <div className="absolute -top-20 -right-20 opacity-60" />,
+      className: "lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-2",
+    },
+    {
+      Icon: HeadsetIcon,
+      name: t("messages.hero-problem.sections.support.title"),
+      description: t("messages.hero-problem.sections.support.description"),
+      href: "/",
+      cta: "Learn more",
+      background: <div className="absolute -top-20 -right-20 opacity-60" />,
+      className: "lg:col-start-1 lg:col-end-2 lg:row-start-2 lg:row-end-3",
+    },
+    {
+      Icon: SwordsIcon,
+      name: t("messages.hero-problem.sections.challenges.title"),
+      description: t("messages.hero-problem.sections.challenges.description"),
+      href: "/",
+      cta: "Learn more",
+      background: <div className="absolute -top-20 -right-20 opacity-60" />,
+      className: "lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-2",
+    },
+    {
+      Icon: BrainIcon,
+      name: t("messages.hero-problem.sections.feedback.title"),
+      description: t("messages.hero-problem.sections.feedback.description"),
+      href: "/",
+      cta: "Learn more",
+      background: <div className="absolute -top-20 -right-20 opacity-60" />,
+      className: "lg:col-start-3 lg:col-end-3 lg:row-start-2 lg:row-end-3",
+    },
+  ];
 
-          <Card className="p-0 col-span-6 shadow-none border-none">
-            <MagicCard
-              gradientColor="oklch(72.3% 0.219 149.579 / .15)"
-              className="p-0 h-full"
-              gradientFrom="oklch(72.3% 0.219 149.579 / .9)"
-              gradientTo="oklch(72.3% 0.219 149.579 / .4)"
-            >
-              <CardContent className="p-4">
-                <div className="relative flex items-center gap-4 mb-4">
-                  <div className="absolute end-0 top-0 size-12 bg-primary/15 rounded-full p-2">
-                    <CompassIcon className="size-8 text-primary" />
-                  </div>
-                  <h3 className="font-semibold text-2xl text-foreground">
-                    {t("messages.hero-problem.sections.learning.title")}
-                  </h3>
-                </div>
-                <p className="ps-1">
-                  {t("messages.hero-problem.sections.learning.description")}
-                </p>
-              </CardContent>
-            </MagicCard>
-          </Card>
-          <Card className="p-0 col-span-4 shadow-none border-none">
-            <MagicCard
-              gradientColor="oklch(72.3% 0.219 149.579 / .15)"
-              className="p-0 h-full"
-              gradientFrom="oklch(72.3% 0.219 149.579 / .9)"
-              gradientTo="oklch(72.3% 0.219 149.579 / .4)"
-            >
-              <CardContent className="p-4">
-                <div className="relative flex items-center gap-4 mb-4">
-                  <div className="absolute end-0 top-0 size-12 bg-primary/15 rounded-full p-2">
-                    <HandshakeIcon className="size-8 text-primary" />
-                  </div>
-                  <h3 className="font-semibold text-2xl text-foreground">
-                    {t("messages.hero-problem.sections.practice.title")}
-                  </h3>
-                </div>
-                <p className="ps-1">
-                  {t("messages.hero-problem.sections.practice.description")}
-                </p>
-              </CardContent>
-            </MagicCard>
-          </Card>
-          <Card className="p-0 col-span-4 shadow-none border-none">
-            <MagicCard
-              gradientColor="oklch(72.3% 0.219 149.579 / .15)"
-              className="p-0 h-full"
-              gradientFrom="oklch(72.3% 0.219 149.579 / .9)"
-              gradientTo="oklch(72.3% 0.219 149.579 / .4)"
-            >
-              <CardContent className="p-4">
-                <div className="relative flex items-center gap-4 mb-4">
-                  <div className="absolute end-0 top-0 size-12 bg-primary/15 rounded-full p-2">
-                    <HeadsetIcon className="size-8 text-primary" />
-                  </div>
-                  <h3 className="font-semibold text-2xl text-foreground">
-                    {t("messages.hero-problem.sections.support.title")}
-                  </h3>
-                </div>
-                <p className="ps-1">
-                  {t("messages.hero-problem.sections.support.description")}
-                </p>
-              </CardContent>
-            </MagicCard>
-          </Card>
-          <Card className="p-0 col-span-4 shadow-none border-none">
-            <MagicCard
-              gradientColor="oklch(72.3% 0.219 149.579 / .15)"
-              className="p-0 h-full"
-              gradientFrom="oklch(72.3% 0.219 149.579 / .9)"
-              gradientTo="oklch(72.3% 0.219 149.579 / .4)"
-            >
-              <CardContent className="p-4">
-                <div className="relative flex items-center gap-4 mb-4">
-                  <div className="absolute end-0 top-0 size-12 bg-primary/15 rounded-full p-2">
-                    <BrainIcon className="size-8 text-primary" />
-                  </div>
-                  <h3 className="font-semibold text-2xl text-foreground">
-                    {t("messages.hero-problem.sections.feedback.title")}
-                  </h3>
-                </div>
-                <p className="ps-1">
-                  {t("messages.hero-problem.sections.feedback.description")}
-                </p>
-              </CardContent>
-            </MagicCard>
-          </Card>
-          <Card className="p-0 col-span-6 shadow-none border-none">
-            <MagicCard
-              gradientColor="oklch(72.3% 0.219 149.579 / .15)"
-              className="p-0 h-full"
-              gradientFrom="oklch(72.3% 0.219 149.579 / .9)"
-              gradientTo="oklch(72.3% 0.219 149.579 / .4)"
-            >
-              <CardContent className="p-4">
-                <div className="relative flex items-center gap-4 mb-4">
-                  <div className="absolute end-0 top-0 size-12 bg-primary/15 rounded-full p-2">
-                    <ChartColumnBigIcon className="size-8 text-primary" />
-                  </div>
-                  <h3 className="font-semibold text-2xl text-foreground">
-                    {t("messages.hero-problem.sections.evaluation.title")}
-                  </h3>
-                </div>
-                <p className="ps-1">
-                  {t("messages.hero-problem.sections.evaluation.description")}
-                </p>
-              </CardContent>
-            </MagicCard>
-          </Card>
-          <Card className="p-0 col-span-6 shadow-none border-none">
-            <MagicCard
-              gradientColor="oklch(72.3% 0.219 149.579 / .15)"
-              className="p-0 h-full"
-              gradientFrom="oklch(72.3% 0.219 149.579 / .9)"
-              gradientTo="oklch(72.3% 0.219 149.579 / .4)"
-            >
-              <CardContent className="p-4">
-                <div className="relative flex items-center gap-4 mb-4">
-                  <div className="absolute end-0 top-0 size-12 bg-primary/15 rounded-full p-2">
-                    <SwordsIcon className="size-8 text-primary" />
-                  </div>
-                  <h3 className="font-semibold text-2xl text-foreground">
-                    {t("messages.hero-problem.sections.challenges.title")}
-                  </h3>
-                </div>
-                <p className="ps-1">
-                  {t("messages.hero-problem.sections.challenges.description")}
-                </p>
-              </CardContent>
-            </MagicCard>
-          </Card>
-        </div>
+  return (
+    <div className="flex gap-6 flex-col items-center justify-center max-w-7xl mx-auto px-4 py-8">
+      <div className="text-center space-y-3">
+        <h2 className="font-bold text-xl text-center sm:text-2xl md:text-3xl leading-relaxed">
+          {t("messages.hero-problem.title")}
+        </h2>
+      </div>
+      <div className="relative z-10 w-full">
+        <BentoGrid className="lg:grid-rows-2">
+          {features.map((feature) => (
+            <BentoCard key={feature.name} {...feature} />
+          ))}
+        </BentoGrid>
       </div>
     </div>
   );
